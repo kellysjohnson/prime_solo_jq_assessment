157d0f21e27d1a4fbf22bfbd7e172579cc2082e5
kellysjohnson <KellyShimkoJohnson@Kellys-MacBook-Pro.local>
2015-04-23 16:25:33 -0500

    Need new solution for button class

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-
+var i = 0;
 
 $(document).ready(function() {
 
-	$(".generate").on("click", function(){
+	$(".generate").click(function(){
 		i++;
-		$(this).append("<div> Line: " + i + "<button class='change'> Change Color </button><button class='remove'> Remove </button></div>");
+		$(".header").append("<div> Line: " + i + "<button class='change'> Change Color </button><button class='remove'> Remove </button></div>");
 
 	});
 
-)};
+	$(".change").click(function(){
+		$(this).css("background-color", "green");
+		console.log("hi!");
+	});
+
+	$(".remove").click(function(){
+		$(this).parent().parent().remove();
+	});
+
+});
+
+
+
+// $(this).replaceWith("<button class='change2'> Change Color </button>");
